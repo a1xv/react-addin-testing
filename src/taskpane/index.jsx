04ff6d6199296d23f15c723a35e7518cad2a0725
Artem Alekseev <aaalekseev@phoenixit.ru>
2023-08-16 02:27:58 +0300

    try 5

--- src/taskpane/index.jsx
+++ src/taskpane/index.jsx
@@ -34,8 +34,8 @@ Office.onReady(() => {
   console.log("KEK");
 });
 
-if ((module as any).hot) {
-  (module as any).hot.accept("./components/App", () => {
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
     const NextApp = require("./components/App").default;
     render(NextApp);
   });
